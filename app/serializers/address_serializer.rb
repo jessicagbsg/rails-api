83ef39f19aa3a2f3347fb7446346fd43f002a0e0
Jessica Gondim <jessicagbsg@gmail.com>
2022-12-17 20:41:49 -0300

    include AMS

---
+++ app/serializers/address_serializer.rb
@@ -0,0 +1,4 @@
+class AddressSerializer < ActiveModel::Serializer
+  attributes :id, :street, :city
+
+end
--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,9 +1,16 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
   
-  belongs_to :kind
+  belongs_to :kind do
+    link(:related) { kind_url(object.kind.id)}
+  end
+
   has_many :phones
   has_one :address
+
+  meta do 
+    { author: "Jessica Gondim"}
+  end
 
   def attributes(*args)
     h = super(*args)

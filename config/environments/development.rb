2cfdb8c85b273e2b9e05d38b6a08c7202f46d101
Jessica Gondim <jessicagbsg@gmail.com>
2022-12-17 20:43:06 -0300

    add default url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,11 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+
+  Rails.application.routes.default_url_options = {
+    host: "localhost",
+    port: 3000
+  }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -52,6 +57,7 @@ Rails.application.configure do
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
+
 
 
   # Raises error for missing translations.

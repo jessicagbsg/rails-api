a475bef1fdd9a761d06c4072f4f67a518130bf33
Jessica Gondim <jessicagbsg@gmail.com>
2022-12-17 21:29:58 -0300

    route contact/:contact_id/phones created

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -2,10 +2,13 @@ class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
   
   belongs_to :kind do
-    link(:related) { kind_url(object.kind.id)}
+    link(:related) { contact_kind_url(object.id)}
   end
 
-  has_many :phones
+  has_many :phones do
+    link(:related) { contact_phones_url(object.id)}
+  end
+
   has_one :address
 
   meta do 

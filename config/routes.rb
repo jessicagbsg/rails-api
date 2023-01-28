45dab9550ee217f952c979db300794fbb6e57d2a
Jessica Gondim <jessicagbsg@gmail.com>
2023-01-28 19:31:51 -0300

    v1 and v2

--- config/routes.rb
+++ config/routes.rb
@@ -3,25 +3,39 @@ Rails.application.routes.draw do
   
 
   resources :auths, only: [:create]
+
   resources :kinds
-  resources :contacts do
-    resource :kind, only: [:show]
-    resource :kind, only: [:show], path: "relationships/kind"
 
-    resource :phones, only: [:show]
-    resource :phones, only: [:show], path: "relationships/phones"
+  scope module: "v1" do
+    resources :contacts, :constraints => lambda { |request| request.params[:version] == "1"} do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: "relationships/kind"
 
-    resource :phone, only: [:update, :create, :destroy]
-    resource :phone, only: [:update, :create, :destroy], path: "relationships/phone"
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: "relationships/phones"
 
-    resource :address, only: [:show, :update, :create, :destroy]
-    resource :address, only: [:show, :update, :create, :destroy], path: "relationships/address"
+      resource :phone, only: [:update, :create, :destroy]
+      resource :phone, only: [:update, :create, :destroy], path: "relationships/phone"
+
+      resource :address, only: [:show, :update, :create, :destroy]
+      resource :address, only: [:show, :update, :create, :destroy], path: "relationships/address"
+    end
   end
 
+  scope module: "v2" do
+    resources :contacts do
+      resource :kind, only: [:show]
+      resource :kind, only: [:show], path: "relationships/kind"
 
+      resource :phones, only: [:show]
+      resource :phones, only: [:show], path: "relationships/phones"
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+      resource :phone, only: [:update, :create, :destroy]
+      resource :phone, only: [:update, :create, :destroy], path: "relationships/phone"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+      resource :address, only: [:show, :update, :create, :destroy]
+      resource :address, only: [:show, :update, :create, :destroy], path: "relationships/address"
+    end
+  end
+
 end
